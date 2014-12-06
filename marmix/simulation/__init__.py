ecf58df19bdc0ccdfb206f7fecc56f7f92c01a15
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2014-12-06 12:34:47 +0100

    Initial simulation scaffolding

---
+++ marmix/simulation/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+# __init__.py
+#
+# Copyright (C) 2014 HES-SO//HEG Arc
+#
+# Author(s): Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
+#
+# This file is part of MarMix.
+#
+# MarMix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MarMix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MarMix. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Simulations are games that are run by a teacher during a specific class. A simulation can be run in parallel with
+an ERPsim game or in standalone mode. This app is used to store the configuration parameters of each simulation.
+"""
+
+
