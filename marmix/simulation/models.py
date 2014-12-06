e9781e4fa0569ee1d2b564107ea94b6f6df6b015
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2014-12-06 21:04:06 +0100

    Simulation model ready to test

--- marmix/simulation/models.py
+++ marmix/simulation/models.py
@@ -21,6 +21,7 @@
 # along with MarMix. If not, see <http://www.gnu.org/licenses/>.
 
 # Stdlib imports
+from hashids import Hashids
 
 # Core Django imports
 from django.db import models
@@ -28,10 +29,16 @@ from django.utils.translation import ugettext_lazy as _
 
 # Third-party app imports
 from django_extensions.db.models import TimeStampedModel
+from shorturls import baseconv
 
 # MarMix imports
 from billing.models import Customer
 from users.models import User
+
+
+def short_code_encode(sim_id, cust_code):
+    encoded_id = baseconv.base32.from_decimal(sim_id)
+    return "%s-%s" % (cust_code.upper(), encoded_id)
 
 
 class Simulation(TimeStampedModel):
@@ -84,12 +91,20 @@ class Simulation(TimeStampedModel):
                                 choices=SIMULATION_STATE_CHOICES, default=CONFIGURING,
                                 help_text=_("The current state of this simulation"))
 
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = 'MM-99999999'
+            super(Simulation, self).save(*args, **kwargs)
+            self.code = short_code_encode(self.id, self.customer.short_code)
+        super(Simulation, self).save(*args, **kwargs)
+
+
     class Meta:
         verbose_name = _('simulation')
         verbose_name_plural = _('simulations')
         ordering = ['code']
 
-    def __unicode__(self):
+    def __str__(self):
         return self.code
 
 
@@ -107,8 +122,34 @@ class Currency(models.Model):
         verbose_name_plural = _('currencies')
         ordering = ['code']
 
-    def __unicode__(self):
+    def __str__(self):
         if self.symbol:
             return self.symbol
         else:
             return self.code
+
+
+# class Team(TimeStampedModel):
+#     """
+#     A team participating in a simulation.
+#     """
+#     simulation = models.ForeignKey('Simulation', verbose_name=_('simulation'), related_name=_('teams'),
+#                                    help_text=_("The simulation in which the team participate"))
+#     login = models.CharField(verbose_name=_("login"), max_length=50, blank=True, null=True,
+#                              help_text=_("A name that can be attributed to the team"))
+#     password = models.
+#     name = models.CharField(verbose_name=_("name"), max_length=50, blank=True, null=True,
+#                             help_text=_("A name that can be attributed to the team"))
+#     team_type =
+#     locked = models.BooleanField
+#
+#     class Meta:
+#         verbose_name = _('team')
+#         verbose_name_plural = _('teams')
+#         ordering = ['simulation', 'login']
+#
+#     def __str__(self):
+#         if self.symbol:
+#             return self.symbol
+#         else:
+#             return self.code
