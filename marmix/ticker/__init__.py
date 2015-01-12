db37408a15a10fb75e1678c45d50157c1b1c247e
Cédric Gaspoz <cedric@gaspoz-fleiner.com>
2015-01-12 13:28:09 +0100

    Added last_quote to Stock

---
+++ marmix/ticker/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: UTF-8 -*-
+# __init__.py
+#
+# Copyright (C) 2014 HES-SO//HEG Arc
+#
+# Author(s): Cédric Gaspoz <cedric.gaspoz@he-arc.ch>
+#
+# This file is part of MarMix.
+#
+# MarMix is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MarMix is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MarMix. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+This is a template app that should get a great new description...
+"""
+
+
