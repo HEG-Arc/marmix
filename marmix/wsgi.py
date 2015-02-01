0f095ae51d6b149575c81afe80e1c4956674661a
MarMix <marmix@m3.marmix.ch>
2015-02-01 14:37:46 +0000

    Production configuration

--- marmix/wsgi.py
+++ marmix/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for  project.
+WSGI config for MarMix.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
@@ -14,20 +14,23 @@ framework.
 
 """
 import os
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.wsgi import get_wsgi_application
 
-# We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
-# if running multiple sites in the same mod_wsgi process. To fix this, use
-# mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = ".settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.production")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
-from configurations.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+class WSGIEnvironment(WSGIHandler):
+
+    def __call__(self, environ, start_response):
+
+        os.environ['MARMIX_TRAVIS_TOKEN'] = environ['MARMIX_TRAVIS_TOKEN']
+        os.environ['MARMIX_TRAVIS_REPO_SLUG'] = environ['MARMIX_TRAVIS_REPO_SLUG']
+        os.environ['MARMIX_SECRET_KEY'] = environ['MARMIX_SECRET_KEY']
+        os.environ['MARMIX_DATABASE_URL'] = environ['MARMIX_DATABASE_URL']
+        os.environ['MARMIX_CACHE_URL'] = environ['MARMIX_CACHE_URL']
+        return super(WSGIEnvironment, self).__call__(environ, start_response)
+
+#application = WSGIEnvironment()
